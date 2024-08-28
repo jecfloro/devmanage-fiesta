921262fe2c2fa8f852bee3855003caefa2662aad
jecfloro <139682508+jecfloro@users.noreply.github.com>
2024-08-29 00:52:51 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -22,10 +22,10 @@ USE `devmanage-fiesta`;
 -- Dumping structure for table devmanage-fiesta.appsysusers
 CREATE TABLE IF NOT EXISTS `appsysusers` (
   `PK_appsysUsers` int NOT NULL AUTO_INCREMENT COMMENT 'AUTO GENERATED ID PER USER',
-  `user_email` int NOT NULL COMMENT 'HASHED EMAIL',
-  `user_password` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'HASHED PASSWORD',
-  `user_empId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'USER EMPLOYEE ID',
-  `user_recoveryCode` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'HASHED RECOVERY CODE',
+  `user_email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'HASHED EMAIL',
+  `user_password` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT 'HASHED PASSWORD',
+  `user_empId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'USER EMPLOYEE ID',
+  `user_recoveryCode` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'HASHED RECOVERY CODE',
   `user_activationCode` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'HASHED ACTIVATION CODE',
   `isActivated` bit(1) DEFAULT NULL,
   `isDisabled` bit(1) DEFAULT NULL,
@@ -36,13 +36,28 @@ CREATE TABLE IF NOT EXISTS `appsysusers` (
   `isCreditCoordinator` bit(1) DEFAULT NULL,
   `isCashier` bit(1) DEFAULT NULL,
   `FK_geolocation` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'GEOLOCATION REFERENCE CODE',
-  `FK_mscinfoChildrens` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `FK_mscinfoRelatives` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `FK_mscinfoNeighbors` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `FK_mscinfoChildrens` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'TABLE MSCINFOCHILDRENS',
+  `FK_mscinfoRelatives` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'TABLE MSCINFORELATIVES',
+  `FK_mscinfoNeighbors` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'TABLE MSCINFONEIGHBORS',
+  `userLastname` varchar(255) DEFAULT NULL,
+  `userFirstName` varchar(255) DEFAULT NULL,
+  `userMiddleName` varchar(255) DEFAULT NULL,
+  `userNickName` varchar(255) DEFAULT NULL,
+  `userGender` bit(1) DEFAULT NULL,
+  `userCivilStatus` varchar(255) DEFAULT NULL,
+  `userNationality` varchar(255) DEFAULT NULL,
+  `userAge` int DEFAULT NULL,
+  `userDateofBirth` date DEFAULT NULL,
+  `userPlaceofBirth` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `userContactNumber` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `userAddress` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `userFullName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`PK_appsysUsers`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
--- Dumping data for table devmanage-fiesta.appsysusers: ~0 rows (approximately)
+-- Dumping data for table devmanage-fiesta.appsysusers: ~1 rows (approximately)
+INSERT INTO `appsysusers` (`PK_appsysUsers`, `user_email`, `user_password`, `user_empId`, `user_recoveryCode`, `user_activationCode`, `isActivated`, `isDisabled`, `isAdmin`, `isCustomer`, `isBranchManager`, `isCreditInvestigator`, `isCreditCoordinator`, `isCashier`, `FK_geolocation`, `FK_mscinfoChildrens`, `FK_mscinfoRelatives`, `FK_mscinfoNeighbors`, `userLastname`, `userFirstName`, `userMiddleName`, `userNickName`, `userGender`, `userCivilStatus`, `userNationality`, `userAge`, `userDateofBirth`, `userPlaceofBirth`, `userContactNumber`, `userAddress`, `userFullName`) VALUES
+	(1, 'administrator@gmail.com', '$2y$10$lyvhvUFJMyXKMlWJnQboz.jhPJhTigzKJ/tvrMD1lKGI5f9Qq1WM.', '230704', 'qkIR62zAoY', 'bPXOrCk0jW3EdsoDBKpuGzHt8iRq2y', b'1', NULL, b'1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'FLORO', 'JECO', 'ESCOTO', 'COCO', b'0', 'SINGLE', 'FILIPINO', 25, '1999-05-11', 'TAGUM CITY', '09389206940', 'DAVAO DEL NORTE COLLEGE COMPOUND, NEW VISAYAS, PANABO CITY', NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
