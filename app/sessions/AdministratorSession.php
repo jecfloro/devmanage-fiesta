013dc40a1051f5131c5e4adff588b7898a8193f6
jecfloro <139682508+jecfloro@users.noreply.github.com>
2024-09-12 20:09:37 +0800

    September 12, 2024

--- app/sessions/AdministratorSession.php
+++ app/sessions/AdministratorSession.php
@@ -2,12 +2,16 @@
 
     include '../connection/MYSQLSERVER.php';
 
-    if ($_SESSION['isLoggedIn'] == 0 && $_SESSION['isDisabled'] == 1 && $_SESSION['isAdmin'] == 0) {
+    if ($_SESSION['isLoggedIn'] == 0 && $_SESSION['isDisabled'] == 1 && $_SESSION['isAdmin'] == NULL) {
         header("Location: /");
     }
 
-    if ($_SESSION['isLoggedIn'] == 0 && $_SESSION['isDisabled'] == 0 && $_SESSION['isAdmin'] == 0) {
+    if ($_SESSION['isLoggedIn'] == 0 && $_SESSION['isDisabled'] == 0 && $_SESSION['isAdmin'] == NULL) {
         header("Location: /");
     }
 
+    if ($_SESSION['isCustomer'] == 1) {
+        header("Location: /");
+    }
+    
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,25 @@
 
 include './app/connection/MYSQLSERVER.php';
 include './app/sessions/UserSession.php';
+require './app/setting/AESCLASS.php';
+
+
+date_default_timezone_set("Asia/Manila");
+$year_start = date("Y");
+$year_end = date("Y") + 1;
+$date = date("Y-m-d");
+$todaysDate = date("Y-m-d H:i:s");
+
+try {
+    $conn = new PDO("mysql:host=$fa_dbserver;dbname=$fa_dbname", $fa_dbuser, $fa_dbpassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $categories = $conn->prepare("SELECT * FROM msc_categories");
+    $categories->execute();
+    $ccategories = $categories->rowCount();
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 
 ?>
 <!DOCTYPE html>
@@ -13,15 +32,16 @@ include './app/sessions/UserSession.php';
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:locale" content="en_US" />
-    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <link rel="shortcut icon" href="../../assets/media/logos/favicon.ico" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-    <link href="assets/plugins/custom/leaflet/leaflet.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/custom.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/plugins/custom/leaflet/leaflet.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/index.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/custom.bundle.css" rel="stylesheet" type="text/css" />
     <script>
         // Frame-busting to prevent site from being loaded within a frame without permission (click-jacking)
         if (window.top != window.self) {
@@ -30,7 +50,7 @@ include './app/sessions/UserSession.php';
     </script>
 </head>
 
-<body id="kt_body" class="auth-bg user-select-none">
+<body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled aside-fixed aside-default-enabled user-select-none">
     <script>
         var defaultThemeMode = "light";
         var themeMode;
@@ -54,64 +74,136 @@ include './app/sessions/UserSession.php';
         }
     </script>
     <div class="d-flex flex-column flex-root">
-        <div class="d-flex flex-column flex-lg-row flex-column-fluid">
-            <div class="d-flex flex-column flex-row-fluid py-10 shadow-lg">
-                <div class="d-flex flex-center flex-column flex-column-fluid">
-                    <div class="w-lg-500px p-10 p-lg-15 mx-auto">
-                        <div class="form w-100">
-                            <a href="/" class="d-flex flex-column align-items-center text-center mb-10">
-                                <img src="./assets/media/logos/FIESTAAPPL_LOGO.png" width="100" alt="">
-                                <h1 class="text-dark fa-2x fw-bolder mb-3 mt-3 text-uppercase">
-                                    Fiesta Appliances
-                                </h1>
+        <div class="page d-flex flex-row flex-column-fluid">
+            <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+                <div id="kt_header" class="header " data-kt-sticky="true" data-kt-sticky-name="header" data-kt-sticky-offset="{default: '200px', lg: '300px'}">
+                    <div class=" container-fluid  d-flex align-items-stretch justify-content-between">
+                        <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
+                            <a href="/" class="d-flex gap-3 align-items-center justify-content-center">
+                                <img alt="Logo" src="../../assets/media/logos/FIESTAAPPL_LOGO.png" width="40px" class="mh-40px theme-light-show" />
+                                <img alt="Logo" src="../../assets/media/logos/FIESTAAPPL_LOGO.png" width="40px" class="mh-40px theme-dark-show" />
+                                <h1 class="mt-3 text-uppercase fw-bolder user_profile">Fiesta Appliances, Inc.</h1>
                             </a>
-                            <div class="fv-row mb-10">
-                                <label class="form-label fs-6 fw-bold text-dark">Email Address</label>
-                                <input class="form-control form-control-lg form-control-solid" type="text" name="user_email" id="user_email" autocomplete="off" placeholder="user@gmail.com" />
-                            </div>
-                            <div class="fv-row mb-5">
-                                <div class="d-flex flex-stack mb-2">
-                                    <label class="form-label fw-bold text-dark fs-6 mb-0">Password</label>
-                                    <a href="forgot-password.php" class="link-primary fs-6 fw-bold">
-                                        Forgot Password ?
-                                    </a>
+                        </div>
+                        <div class="d-flex align-items-stretch justify-content-end flex-lg-grow-1">
+                            <div class="d-flex align-items-stretch flex-shrink-0">
+                                <div class="d-flex align-items-center ms-1 ms-lg-2">
+                                    <a href="#" class="btn btn-icon btn-active-light-primary w-30px h-30px w-md-40px h-md-40px" data-kt-menu-trigger="{default:'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+                                        <i class="ki-duotone ki-night-day theme-light-show fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span></i> <i class="ki-duotone ki-moon theme-dark-show fs-1"><span class="path1"></span><span class="path2"></span></i></a>
+                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-title-gray-700 menu-icon-gray-500 menu-active-bg menu-state-color fw-semibold py-4 fs-base w-150px" data-kt-menu="true" data-kt-element="theme-mode-menu">
+                                        <div class="menu-item px-3 my-0">
+                                            <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="light">
+                                                <span class="menu-icon" data-kt-element="icon">
+                                                    <i class="ki-duotone ki-night-day fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span></i> </span>
+                                                <span class="menu-title">
+                                                    Light
+                                                </span>
+                                            </a>
+                                        </div>
+                                        <div class="menu-item px-3 my-0">
+                                            <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="dark">
+                                                <span class="menu-icon" data-kt-element="icon">
+                                                    <i class="ki-duotone ki-moon fs-2"><span class="path1"></span><span class="path2"></span></i> </span>
+                                                <span class="menu-title">
+                                                    Dark
+                                                </span>
+                                            </a>
+                                        </div>
+                                        <div class="menu-item px-3 my-0">
+                                            <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="system">
+                                                <span class="menu-icon" data-kt-element="icon">
+                                                    <i class="ki-duotone ki-screen fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i> </span>
+                                                <span class="menu-title">
+                                                    System
+                                                </span>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <input class="form-control form-control-lg form-control-solid" type="password" name="user_password" id="user_password" autocomplete="off" placeholder="****" />
-                            </div>
-                            <div class="d-flex justify-content-center align-items-center mb-10">
-                                <div class="text-gray-400 fw-semibold fs-6">
-                                    New Here?
-                                    <a href="/register.php" class="link-primary fw-bold">
-                                        Create an Account
-                                    </a>
+                                <div class="d-flex align-items-center ms-2 ms-lg-3">
+                                    <a href="login.php" class="btn btn-primary"> Log in / Sign up </a>
                                 </div>
-                            </div>
-                            <div class="text-center">
-                                <button class="btn btn-lg btn-darkgreen w-100 mb-5" id="authBtnLogin_submit">
-                                    <span class="indicator-label">
-                                        Continue
-                                    </span>
-                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="d-flex flex-column flex-lg-row-auto w-xl-400px positon-xl-relative shadow-sm mainpage-container-side">
-                <div class="d-flex flex-column justify-content-between">
-                    <div class="d-flex flex-row-fluid flex-column text-center p-5">
-                        <div class="mainpage-logo-side d-flex justify-content-end flex-column text-center gap-5">
-                            <img class="" src="./assets/media/logos/FIESTAAPPL_LOGO_ALT.png" width="100" alt="">
-                            <p class="mt-n3 text-white">Version 1.0.0</p>
+                <div class="content fs-6 d-flex flex-column flex-column-fluid" id="kt_content">
+                    <div class="toolbar" id="kt_toolbar">
+                        <div class=" container-fluid  d-flex flex-stack flex-wrap flex-sm-nowrap">
+                            <div class="d-flex align-items-center flex-nowrap text-nowrap py-1">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="post fs-6 d-flex flex-column-fluid" id="kt_post">
+                        <div class="container-fluid">
+                            <div class="row g-xl-12">
+                                <div class="col-xxl-12">
+                                    <div class="card">
+                                        <div class="card-body d-flex justify-content-between flex-wrap">
+                                            <div class="">
+                                                <div data-kt-search-element="div" class="w-250px position-relative mb-5 mb-lg-0" autocomplete="off">
+                                                    <!--begin::Hidden input(Added to disable form autocomplete)-->
+                                                    <input type="hidden">
+                                                    <!--end::Hidden input-->
+
+                                                    <!--begin::Icon-->
+                                                    <i class="ki-duotone ki-magnifier search-icon fs-2 text-gray-500 position-absolute top-50 translate-middle-y ms-5"><span class="path1"></span><span class="path2"></span></i> <!--end::Icon-->
+
+                                                    <!--begin::Input-->
+                                                    <input type="text" class="search-input form-control form-control-solid ps-13" name="search" value="" placeholder="Search..." data-kt-search-element="input">
+                                                    <!--end::Input-->
+
+                                                    <!--begin::Spinner-->
+                                                    <span class="search-spinner  position-absolute top-50 end-0 translate-middle-y lh-0 d-none me-5" data-kt-search-element="spinner">
+                                                        <span class="spinner-border h-15px w-15px align-middle text-gray-400"></span>
+                                                    </span>
+                                                    <!--end::Spinner-->
+
+                                                    <!--begin::Reset-->
+                                                    <span class="search-reset  btn btn-flush btn-active-color-primary position-absolute top-50 end-0 translate-middle-y lh-0 d-none me-4" data-kt-search-element="clear">
+                                                        <i class="ki-duotone ki-cross fs-2 fs-lg-1 me-0"><span class="path1"></span><span class="path2"></span></i> </span>
+                                                    <!--end::Reset-->
+                                                </div>
+                                            </div>
+                                            <div class="">
+                                                <select class="form-select form-select-solid fw-bolder w-250px" data-kt-select2="true" data-placeholder="Select Sort" data-allow-clear="true" data-kt-user-table-filter="usertype" data-hide-search="true" id="ii_sorting">
+                                                    <option>Default Sorting</option>
+                                                    <option>Sort by price: Low to High</option>
+                                                    <option>Sort by price: High to Low</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer py-4 d-flex flex-lg-column user-select-none" id="kt_footer">
+                    <div class="container-fluid d-flex flex-column justify-content-end flex-md-row flex-stack">
+                        <div class="text-dark order-2 order-md-1">
+                            <span class="text-muted fw-semibold me-1">
+                                <script>
+                                    document.write(new Date().getFullYear());
+                                </script> &copy;
+                            </span>
+                            <span class="text-gray-800 text-hover-primary">Fiesta Appliances, Inc.</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
+        <i class="ki-duotone ki-arrow-up"><span class="path1"></span><span class="path2"></span></i>
+    </div>
     <script src="../../assets/plugins/global/plugins.bundle.js"></script>
     <script src="../../assets/js/scripts.bundle.js"></script>
-    <script type="module" src="./app/js/main.authenticationScript.js"></script>
+    <script src="../../assets/plugins/custom/leaflet/leaflet.bundle.js"></script>
+    <script src="../../assets/plugins/custom/datatables/datatables.bundle.js"></script>
+    <script src="../../assets/js/widgets.bundle.js"></script>
+    <script src="../../assets/js/custom/widgets.js"></script>
 </body>
 
 </html>
